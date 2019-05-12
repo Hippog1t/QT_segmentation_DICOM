7acfbf096882053428a8d971c9a133cdfde8dc76
AlexLacour <alexandre.lacour3@hotmail.fr>
2019-05-12 16:41:06 +0200

    final

--- waterShed.py
+++ waterShed.py
@@ -15,15 +15,15 @@ outFile = sys.argv[2]+"/Out/waterShed.jpg"
 
 img = sitk.ReadImage(inputFile)
 
-mpimg.imsave("temp_lpe.jpg", np.squeeze(sitk.GetArrayFromImage(img)), cmap="gray")
+mpimg.imsave(sys.argv[2]+"/Out/tempws.jpg", np.squeeze(sitk.GetArrayFromImage(img)), cmap="gray")
 
-temp_img = sitk.ReadImage("temp_lpe.jpg")
+temp_img = sitk.ReadImage(sys.argv[2]+"/Out/tempws.jpg")
 
 img = sitk.VectorIndexSelectionCast(temp_img,1)
 
 feature_img = sitk.GradientMagnitudeRecursiveGaussian(img, sigma=2)
 
-ws_img = sitk.MorphologicalWatershed(feature_img, level=4, markWatershedLine=False, fullyConnected=False)
+ws_img = sitk.MorphologicalWatershed(feature_img, level=2, markWatershedLine=True, fullyConnected=False)
 
 #seg = sitk.ConnectedComponent(ws_img!=ws_img[0,256])
 final_img = sitk.LabelOverlay(img, ws_img)
