e3e0a3ebc8a22ae353f91dbe202838e9dec6b6a6
AlexLacour <alexandre.lacour3@hotmail.fr>
2019-05-15 19:12:22 +0200

    FINAL + new colormap

--- link_py/mainwindow.cpp
+++ link_py/mainwindow.cpp
@@ -24,10 +24,14 @@ MainWindow::MainWindow(QWidget *parent) :
     isCT = false;
     isMR = false;
 
+    //Location of the .exe file
     path = QCoreApplication::applicationDirPath();
+
+    //Window Parameters
     this->setWindowIcon(QIcon(QPixmap(path+"/Ressources/icone.jpg")));
     this->setWindowTitle(QString("Medical Image Segmentation - ESME 2019"));
 
+    //Links the buttons to the functions
     connect(ui->rgButton, SIGNAL(clicked()), this, SLOT(regionGrowing()));
     connect(ui->lpeButton, SIGNAL(clicked()), this, SLOT(waterShedSeg()));
     connect(ui->clearButton, SIGNAL(clicked()), this, SLOT(reset()));
@@ -41,7 +45,7 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(ui->actionRGB, SIGNAL(triggered()), this, SLOT(applyrgb()));
     connect(ui->actionSpectral, SIGNAL(triggered()), this, SLOT(applyspectral()));
     connect(ui->actionBlue_and_red, SIGNAL(triggered()), this, SLOT(applyblueandred()));
-    connect(ui->actionBlue_and_red, SIGNAL(triggered()), this, SLOT(applyRGB2()));
+    connect(ui->actionRGB2, SIGNAL(triggered()), this, SLOT(applyRGB2()));
     connect(ui->actionThresholds, SIGNAL(triggered()), this, SLOT(applythresholds()));
     connect(ui->actionGreyscale, SIGNAL(triggered()), this, SLOT(reset()));
     connect(ui->actionPrism, SIGNAL(triggered()), this, SLOT(applyprism()));
@@ -49,8 +53,9 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(ui->thresholdButton, SIGNAL(clicked()), this, SLOT(Thresholding()));
     connect(ui->enterValue, SIGNAL(textChanged(QString)), this, SLOT(changeofValue()));
 
-
+    //This will resize the pictures to fit the pictureLabel
     ui->picture->setScaledContents( true );
+
     QPixmap pixmap(path+"/Ressources/accueil.jpg");
     ui->picture->setPixmap(pixmap);
 
@@ -70,9 +75,11 @@ MainWindow::~MainWindow()
 void MainWindow::importdir(){
     dirpath = QFileDialog::getExistingDirectory(this, tr("Select Directory"), "C:/", QFileDialog::ShowDirsOnly);
     if(QDir(dirpath).exists()){
+        //files is a QStringList containing the names of the files in the directory selected
         files.clear();
         callInit(dirpath);
 
+        //callInit created a directory.txt, to be copied in the QStringList files
         QFile dir(QString(path+"/Out/directory.txt"));
         dir.open(QIODevice::ReadOnly);
         QTextStream in(&dir);
@@ -84,12 +91,12 @@ void MainWindow::importdir(){
         }
         dir.close();
 
+        //Parameters of the slider (size, value of increment...)
         ui->selectFileDir->setRange(0, files.length()-1);
         ui->selectFileDir->setTickInterval(1);
         ui->selectFileDir->setEnabled(true);
         ui->filenumber->setNum(1);
         ui->totalfiles->setText("/ "+QString::number(files.length()));
-
 
         ui->selectFileDir->setValue(0);
         int v = ui->selectFileDir->value();
@@ -104,8 +111,10 @@ void MainWindow::importdir(){
         picWidth = pixmap.width();
         picHeight = pixmap.height();
         ui->picture->setPixmap(pixmap);
+        //To see the dicom header in a table
         displayHeader();
 
+        //Now that there's a picture, we can allow the segmentations
         thereIsPicture = true;
         regionGrow = false;
         waterShed = false;
@@ -113,6 +122,7 @@ void MainWindow::importdir(){
         Threshold = false;
         ui->enterValue->setEnabled(true);
 
+        //To check if the picture is a scanner or a MRI
         CTorMR();
     }
 }
@@ -120,8 +130,10 @@ void MainWindow::importdir(){
 void MainWindow::importdcm(){
     filepath = QFileDialog::getOpenFileName(this,tr("Select picture"),"C:/",tr("Dicom Files (*.dcm)"));
     if(QFile(filepath).exists()){
+        //To get the name of the file selected
         QFileInfo f(filepath);
         filename = f.baseName();
+
         ui->filepath->setText(filepath);
         ui->filename->setText(filename);
         ui->selectFileDir->setEnabled(false);
@@ -149,16 +161,21 @@ void MainWindow::importdcm(){
 }
 
 void MainWindow::goThroughFile(){
+    //This function handles the movements of the slider
     int value = ui->selectFileDir->value();
     ui->filenumber->setNum(value+1);
+
     QPixmap pixmap(path+"/Out/initial"+QString::number(value)+".jpg");
     picWidth = pixmap.width();
     picHeight = pixmap.height();
     ui->picture->setPixmap(pixmap);
+
     filename = files[value];
     filepath = dirpath+"/"+filename;
+
     ui->filename->setText(filename);
     ui->filepath->setText(filepath);
+
     thereIsPicture = true;
     regionGrow = false;
     waterShed = false;
@@ -166,6 +183,7 @@ void MainWindow::goThroughFile(){
 }
 
 void MainWindow::reset(){
+    //Cancel the segmentation
     QPixmap pixmap_accueil(path+"/Ressources/accueil.jpg");
     ui->picture->setPixmap(pixmap_accueil);
 
@@ -186,23 +204,26 @@ void MainWindow::regionGrowing(){
     ui->errors->clear();
     if(!regionGrow && thereIsPicture && thereIsSeed){
         callRegionGrow();
+        //The region growing segmentation has been done => regionGrow = true
         regionGrow = true;
     }
 
     if(regionGrow){
+        //If the operation has been done once and the seed has not been changed, we can simply reload the picture
         QPixmap pixmap(path+"/Out/regionGrow.jpg");
         ui->picture->setPixmap(pixmap);
         ui->picture->show();
     }
 
     if(!thereIsSeed){
-        ui->errors->setText("Erreur : Vous devez indiquer une valeur de seed\nCliquez sur l'image");
+        ui->errors->setText("Error : You have to select a seed\nClick on the picture");
     }
 }
 
 void MainWindow::waterShedSeg(){
     if(!waterShed && thereIsPicture){
         callWaterShed();
+        //The watershed segmentation has been done => watershed = true
         waterShed = true;
     }
 
@@ -223,6 +244,7 @@ void MainWindow::Thresholding(){
         callThreshold(ui->enterValue->text());
         QPixmap pixmap(path+"/Ressources/chargement.jpg");
         ui->picture->setPixmap(pixmap);
+        //The thresholding segmentation has been done => threshold = true
         Threshold = true;
     }
 
@@ -233,19 +255,23 @@ void MainWindow::Thresholding(){
     }
 
     if(!thereIsValue){
-        ui->errors->setText("Erreur : Vous devez indiquer une valeur de seuillage");
+        ui->errors->setText("Error : Select a threshold value");
     }
 }
 
 void MainWindow::mouseReleaseEvent(QMouseEvent* event){
     if (event->button() == Qt::LeftButton && thereIsPicture){
+        //mapFromParent gives the coordinates inside of the widget, mapFromGlobal gives the coordinates inside the window
         QPoint point = ui->picture->mapFromParent(event->pos());
-        if(point.x()<=ui->picture->width() && point.x()>=0 && point.y()<=ui->picture->height()+15 && point.y()>=15){
+
+        if(point.x()<=ui->picture->width() && point.x()>=0 && point.y()<=ui->picture->height()+20 && point.y()>=20){
+            //There's a constant error of 20px for the y-axis, the +20 is there to correct it
             ui->x_pos->setNum(int(point.x()*picWidth/1024));
-            ui->y_pos->setNum(int(point.y())*picHeight/1024-15);
+            ui->y_pos->setNum(int(point.y())*picHeight/1024-10);
             regionGrow = false;
             thereIsSeed = true;
         }
+
         else{
             ui->x_pos->setText(QString(""));
             ui->y_pos->setText(QString(""));
@@ -255,6 +281,7 @@ void MainWindow::mouseReleaseEvent(QMouseEvent* event){
 }
 
 void MainWindow::closeEvent(QCloseEvent *event){
+    //deletes the content of the Out directory when the window is closed
     QDir dir(path+"/Out/");
     foreach(QString dirFile, dir.entryList())
     {
@@ -264,6 +291,7 @@ void MainWindow::closeEvent(QCloseEvent *event){
 }
 
 void MainWindow::displayHeader(){
+    //Reads the content of the dicomheader.txt file and put it in the table
     ui->headerView->clear();
     callExtractHeader(filepath);
 
@@ -288,6 +316,7 @@ void MainWindow::save(){
     ui->picture->grab().save(imagePath);
 }
 
+//Themes of the application
 void MainWindow::setDarkTheme(){
     ui->errors->setStyleSheet("QLabel { color : red; }");
 
@@ -303,6 +332,7 @@ void MainWindow::setLightTheme(){
     this->setStyleSheet("");
 }
 
+//Colormaps
 void MainWindow::applyhsv(){
     applyColormap("hsv");
     QPixmap pixmap(path+"/Out/lsd.jpg");
@@ -345,6 +375,7 @@ void MainWindow::applyRGB2(){
     ui->picture->setPixmap(pixmap);
 }
 
+//Loads a demo dcm file
 void MainWindow::demonstration(){
     filepath = path+"/Ressources/demo.dcm";
     ui->filepath->setText(filepath);
@@ -369,6 +400,7 @@ void MainWindow::demonstration(){
 }
 
 void MainWindow::CTorMR(){
+    //Uses the dicom header to know if the picture is a scanner or a MRI
     QTreeWidgetItem* a = ui->headerView->findItems(QString("Modality"), Qt::MatchFlag::MatchExactly, 0)[0];
     QString modality = a->text(1);
     if(modality == "CT"){
@@ -383,6 +415,7 @@ void MainWindow::CTorMR(){
 }
 
 void MainWindow::changeofValue(){
+    //Makes sure that the threshold is relevant with the type of picture
     thereIsValue = true;
     Threshold = false;
     if(isCT){
@@ -407,7 +440,9 @@ void MainWindow::changeofValue(){
     }
 }
 
+//Calls of the python scripts
 void MainWindow::callInit(QString pictures){
+    //Converts to jpg and saves the picture to Out
     QString program = "python";
     QStringList args;
     args << path+"/init.py" << pictures << path;
@@ -417,6 +452,7 @@ void MainWindow::callInit(QString pictures){
 }
 
 void MainWindow::callExtractHeader(QString picture){
+    //Creates dicomheader.txt and directory.txt if a directory is selected
     QString program = "python";
     QStringList args;
     args << path+"/extractHeader.py" << picture << path;
